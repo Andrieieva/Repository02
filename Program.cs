4549e251b95c8e7f70ff3806c2ef910821d88801
Nataliia Andrieieva <natalia__forever@ukr.net>
2023-09-04 14:43:25 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please, enter the number:");
+        double num = Convert.ToDouble(Console.ReadLine());
+        double circumferenceOfCircle = 2 * Math.PI * num;
+        Console.WriteLine("The result is: " + circumferenceOfCircle);
+
+    }
+}
